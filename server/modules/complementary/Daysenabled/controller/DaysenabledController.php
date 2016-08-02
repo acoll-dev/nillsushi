c61e6e296d5badc47fc219363bac8693d68339e8
Felipe Augusto de Oliveira <felipe_augusto159@hotmail.com>
2016-08-01 22:32:51 -0300

    solved commit

--- server/modules/complementary/Daysenabled/controller/DaysenabledController.php
+++ server/modules/complementary/Daysenabled/controller/DaysenabledController.php
@@ -58,14 +58,12 @@ class DaysenabledController {
                     if($find){
                         $array['enabled'] = 1;
                         $find->update_attributes($array);
-                        $APP->activityLog("Update Days Enabled");
                     }else{
                         Daysenabled::create($array);
-                        $APP->activityLog("Register Days Enabled");
                     }
 
                     $APP->CON->commit();
-                    
+                    $APP->activityLog("Register Days Enabled");
                     return $APP->response(true, 'success', 'SUCCESS.CREATE.COMPLETED', $json);
                 } else {
                     throw new Exception("There are no values for registering!");
